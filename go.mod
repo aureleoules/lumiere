c44c4ab787d9fda220c1dff1e59ae313a915a769
Aurèle Oulès <aurele@oules.com>
2020-05-29 03:35:56 +0200

    serve app

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/aureleoules/lumiere
 
 go 1.14
+
+require (
+	github.com/gin-gonic/contrib v0.0.0-20191209060500-d6e26eeaa607
+	github.com/gin-gonic/gin v1.6.3
+)
--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	// Production
+	router.Use(static.Serve("/", static.LocalFile("./ui/build", true)))
+
+	api := router.Group("/api")
+	api.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run(":3000")
 }
